df3de6a329bb084de6f7f8b328ffb8aa0d8e6617
Aaron <aaronbreault@gmail.com>
2025-08-07 14:33:10 -0700

    cleaning up burst_store. working on adding Create New Database functionality.  mostly saving work to move working to another computer.

--- aes256cbc/burst_store.pl
+++ aes256cbc/burst_store.pl
@@ -37,14 +37,8 @@ if ($q->param){
 	print $fh "STORE BURST\n";
 	print $fh "$time\n";
 	print $fh "$ip\n";
-	#print $fh "fingerprint: $fingerprint\n";
-	#print $fh "data: $data\n";
-	#foreach my $record (@{$hash_ref->{records}}) {
-	  #print $fh "fingerprint: $record->{fingerprint}\ndata: $record->{data}\n\n";
-	#}
-	#print $q->header();
-	#print qq{{"response":"test success"}};
-	#exit 0;
+
+	# Gather data for a report that isn't gigantic.
 	my $num_dummy_data = 0;
 	my $num_new_records = 0;
 	my $num_updated_records = 0;
@@ -56,8 +50,8 @@ if ($q->param){
 	foreach my $record (@{$hash_ref->{records}}) {
 		# Count number of total records.
 		$num_records++;
-		# Loop through dummy data until we find a valid fingerprint.
-		if( $go_flag == 1 || ($total_records - $num_dummy_data) == 1 ){
+		#if( $go_flag == 1 || ($total_records - $num_dummy_data) == 1 ){
+		if( $go_flag == 1 ){
 			# Now we can try inserting first. Will return empty if the record already exists.
 			$response = $dbh->do("INSERT INTO $db_table (fingerprint, data, ip) VALUES (?, ?, ?)", undef, $record->{fingerprint}, $record->{data}, $ip);
 			if(not defined $response){
@@ -68,10 +62,7 @@ if ($q->param){
 				# Record didn't exist, increment count.
 				$num_new_records++;
 			}
-		# Case where all dummy data except for 1 single good record at the end. First burst!
-			#$response = $dbh->do("INSERT INTO $db_table (fingerprint, data, ip) VALUES (?, ?, ?)", undef, $record->{fingerprint}, $record->{data}, $ip);
-			#$num_new_records++;
-		#}elsif( $go_flag == 0 ){
+		# Loop through dummy data until we find a valid fingerprint.
 		}else{
 			$response = $dbh->do("UPDATE $db_table SET data=?, ip=? WHERE fingerprint=?", undef, $record->{data}, $ip, $record->{fingerprint}) or die $dbh->errstr;
 			# Switch to good data if we found updateable edge.
@@ -84,6 +75,7 @@ if ($q->param){
 			}
 		}
 	}
+
 	$dbh->disconnect();
 	print $fh "total_records: $total_records\n";
 	print $fh "num_records, counted: $num_records\n";
@@ -96,69 +88,6 @@ if ($q->param){
 }
 
 
-
-=pod
-	if($test_fingerprint =~ /^[0-9a-f]{64}$/i){
-		print "fingerprint matched\n";
-	}else{
-		print "fingerprint not matched\n";
-	}
-
-	if($test_data =~ /^[1-9A-HJ-NP-Za-km-z]{1000}$/){
-		print "data matched\n";
-	}else{
-		print "data not matched\n";
-	}
-
-# testing - print fingerprints
-	# ASC for ascending, DESC for descending
-	my $dbh = DBI->connect("dbi:MariaDB:$db_name", $db_username, $db_pw);
-	my $query = "SELECT fingerprint FROM $db_table";
-	#my $query = "SELECT sender, timestamp, content, screenname FROM $db_table_chat_messages WHERE chat_id=$chat_id ORDER BY timestamp ASC";
-	my $sth = $dbh->prepare($query);
-	$sth->execute();
-	# Fill chat message array with rows containing an internal hash
-	my @chat_data_from_sql;
-	my $internal_hash;
-	while( my $row = $sth->fetchrow_hashref ){
-		print "fp: $row->{fingerprint}\n";
-
-	}
-
-	my $dbh = DBI->connect("dbi:MariaDB:$db_name", $db_username, $db_pw);
-	#$dbh->do("UPDATE $db_table_misc_data SET name=?, value=? WHERE name='$name'", undef, $name, $value);
-#"INSERT INTO $db_table_contact_messages (ip, message) VALUES (?, ?)", undef, $ip, $message
-	$dbh->do("INSERT INTO $db_table (fingerprint, data) VALUES (?, ?)", undef, $value, $data);
-	$dbh->disconnect();
-
-#save sub core
-if ($q->param){
-
-	# Decode to hash ref to extract user_id and subscription
-	my $hash_ref = $json->decode($q->param('POSTDATA'));
-	my $user_id = $hash_ref->{id};
-	my $subscription = $hash_ref->{sub};
-
-	my $dbh = DBI->connect("dbi:MariaDB:$db_name", $db_username, $db_pw);
-	#$dbh->do("UPDATE $db_table_misc_data SET name=?, value=? WHERE name='$name'", undef, $name, $value);
-	$dbh->do("UPDATE $db_table_user_id SET subscription=? WHERE user_id='$user_id'", undef, $subscription);
-	$dbh->disconnect();
-
-	# Notify of success or failure
-	print $q->header();
-	print '{"message":"success"}';;
-}
-
-#logging
-	# log user_id, message, and subscription for each time this 
-	my $filename = './log.txt';
-	open(my $fh, '>>', $filename) or die;
-	print $fh "browser: $browser\n";
-	print $fh "time: $time\n";
-	print $fh "user_id: $user_id\n";
-	print $fh "message: $msg\n";
-
-=cut
 
 
 
