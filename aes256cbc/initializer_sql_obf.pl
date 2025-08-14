c5d99edf8a969e293318fc58cbe2265f2173ab46
Aaron <aaronbreault@gmail.com>
2025-08-14 13:09:54 -0700

    initializer now works on a test system assuming you have mysql user testuser with password set up correctly. deleting old index file that will soon be replaced with obfuscate.tmpl + obfuscate.pl combo

--- aes256cbc/initializer_sql_obf.pl
+++ aes256cbc/initializer_sql_obf.pl
@@ -4,31 +4,27 @@ use warnings;
 
 use DBI;
 
-# Manually set up username/password first (sandbox side)
-#my $db_username = 'qrconnect';
-#my $db_pw = 'testqrconnect';
-#my $db_name = 'chatdance';
-#my $db_table_chat_rooms = 'chat_rooms';
-#my $db_table_chat_messages = 'chat_messages';
-#my $db_table_contact_messages = 'contact_messages';
-
 ##########################################################
 # This script kept mostly as a reference to SQL structure.
 ##########################################################
 
+# Set username/password (sandbox side)
+my $db_username = 'testuser';
+my $db_pw = 'password';
 # Set SQL server/production variables
-my $db_username = 'chatriwe_admin';
-my $db_pw = 'Vuu_fQY1#qH,';
+#my $db_username = 'chatriwe_admin';
+#my $db_pw = 'Vuu_fQY1#qH,';
+# Database + Table names should be the same
 my $db_name = 'chatriwe_obf';
 my $db_table_obf = 'obfuscation';
-my $db_table_offline_dbase = 'offline_dbase';
 
 # Create database
-my $dbh = DBI->connect('dbi:MariaDB:', $db_username, $db_pw);
+#my $dbh = DBI->connect('dbi:MariaDB:', $db_username, $db_pw);
+my $dbh = DBI->connect('dbi:mysql:', $db_username, $db_pw);
 $dbh->do("CREATE DATABASE IF NOT EXISTS $db_name");
-$dbh->disconnect;
 
-$dbh = DBI->connect("dbi:MariaDB:$db_name", $db_username, $db_pw);
+#$dbh = DBI->connect("dbi:MariaDB:$db_name", $db_username, $db_pw);
+$dbh = DBI->connect("dbi:mysql:$db_name", $db_username, $db_pw);
 # Create table to hold URL and fisher user data
 $dbh->do("CREATE TABLE IF NOT EXISTS $db_table_obf (fingerprint VARCHAR(64), data VARCHAR(1000), ip VARCHAR(16), timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(fingerprint) )"); 
 
