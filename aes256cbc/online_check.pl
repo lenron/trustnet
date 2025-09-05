9005723e1ae9d091951af62e3b4bffe7f769879a
Aaron <aaronbreault@gmail.com>
2025-09-05 11:41:23 -0700

    Online check now working for both main online site and standalone.html using CORS (which is working again!). Needed a perl endpoint instead of GETing index.html to get CORS working (not sure why).

---
+++ aes256cbc/online_check.pl
@@ -0,0 +1,39 @@
+#!/home/chatriwe/perl5/perlbrew/perls/perl-5.10.1/bin/perl5.10.1
+use strict;
+use warnings;
+#use cPanelUserConfig;
+
+use Time::Piece;
+use CGI qw(:standard escapeHTML);
+use DBI;
+use URI::Escape;
+use JSON;
+
+# Create CGI and json objects.
+my $q = CGI->new;
+my $json = JSON->new;
+
+	# Get user ip.
+	my $ip = $ENV{REMOTE_ADDR};
+	#my $ip = "1.1.1.1";
+
+	# Get current time for log.
+	my $t = localtime;
+	my $time = $t->strftime();
+	# If log exists, we know q->param caught data.
+	my $filename = './online_check_log.txt';
+	# Append to existing file if it exists, create new otherwise.
+	open(my $fh, '>>', $filename); # or die;
+	print $fh "\n\nONLINE CHECK\n";
+	print $fh "$time\n";
+	print $fh "$ip\n";
+
+	my $response = "success";
+	print $q->header();
+	print qq{{"response":"$response"}};
+}
+
+
+
+
+
