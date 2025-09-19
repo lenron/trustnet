c3b329797049b6fd0fe164c4da4fb88e5d7a1cff
Aaron <aaronbreault@gmail.com>
2025-09-19 02:55:07 -0700

    README explains how to launch obf with a oneliner and docker. Also included are instructions and code for installing a running instance of obf in a container using a one-liner bash script (after installing docker). This will install apache2, perl, our needed perl modules, as well as enable all required apache modules such as cgi, rewrite (for .htaccess switches), headers, expires to our custom image which will be run as a container. The bash script runs the Dockerfile which installs all the files to an image that when launched that runs separate from the linux kernel.

---
+++ aes256cbc/build_docker_image_and_launch_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+IMAGE=${IMAGE:-"myobf"}
+# make a function to build the image
+build_image() {
+  IMAGE_NAME=$1
+  DOCKERFILE=$2
+
+  echo "Building image. IMAGE=$IMAGE_NAME"
+  docker build . \
+    --file $DOCKERFILE \
+    -t "$IMAGE_NAME"
+}
+
+build_image $IMAGE Dockerfile
+
+docker run -dit --name runmyobf -p 7888:80 myobf
