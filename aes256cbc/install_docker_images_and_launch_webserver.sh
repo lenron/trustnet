6912405b0ac62c526cf31763c826dd7759415c5e
Aaron <aaronbreault@gmail.com>
2025-09-21 01:59:25 -0700

    Docker image install and run script tested as working. Also, assuming the user will acquire this script from this repo, we don't need to download and then move into this repo.

--- aes256cbc/install_docker_images_and_launch_webserver.sh
+++ aes256cbc/install_docker_images_and_launch_webserver.sh
@@ -20,12 +20,6 @@ sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Install repo
-apt-get install -y git
-git clone https://github.com/lenron/trustnet.git
-
-# Move into repo working directory. This dir comes pre-enabled for perl cgi scripting.
-cd ~/trustnet/aes256cbc
 # Run docker compose file compose.yaml. This fill will download images and run the httpd (apache) and MariaDB containers in the background.
 sudo docker compose up -d
 
