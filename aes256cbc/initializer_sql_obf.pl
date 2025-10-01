4482c4ed7db64d50ba15ed1f250a04e496c81cc3
Aaron <aaronbreault@gmail.com>
2025-09-30 17:48:50 -0700

    Turning off lines of code in a script meant for use as reference.

--- aes256cbc/initializer_sql_obf.pl
+++ aes256cbc/initializer_sql_obf.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-use DBI;
+#use DBI;
 
 ##########################################################
 # This script kept mostly as a reference to SQL structure.
@@ -12,23 +12,23 @@ use DBI;
 #my $db_username = 'testuser';
 #my $db_pw = 'password';
 # Set SQL server/production variables
-my $db_username = 'chatriwe_admin';
-my $db_pw = 'Vuu_fQY1#qH,';
+#my $db_username = 'chatriwe_admin';
+#my $db_pw = 'Vuu_fQY1#qH,';
 # Database + Table names should be the same
-my $db_name = 'chatriwe_obf';
-my $db_table_obf = 'obfuscation';
+#my $db_name = 'chatriwe_obf';
+#my $db_table_obf = 'obfuscation';
 
 # Create database
 #my $dbh = DBI->connect('dbi:MariaDB:', $db_username, $db_pw);
-my $dbh = DBI->connect('dbi:mysql:', $db_username, $db_pw);
-$dbh->do("CREATE DATABASE IF NOT EXISTS $db_name");
+#my $dbh = DBI->connect('dbi:mysql:', $db_username, $db_pw);
+#$dbh->do("CREATE DATABASE IF NOT EXISTS $db_name");
 
 #$dbh = DBI->connect("dbi:MariaDB:$db_name", $db_username, $db_pw);
-$dbh = DBI->connect("dbi:mysql:$db_name", $db_username, $db_pw);
+#$dbh = DBI->connect("dbi:mysql:$db_name", $db_username, $db_pw);
 # Create table to hold URL and fisher user data
-$dbh->do("CREATE TABLE IF NOT EXISTS $db_table_obf (fingerprint VARCHAR(64), data VARCHAR(1000), ip VARCHAR(16), timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(fingerprint) )"); 
+#$dbh->do("CREATE TABLE IF NOT EXISTS $db_table_obf (fingerprint VARCHAR(64), data VARCHAR(1000), ip VARCHAR(16), timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(fingerprint) )"); 
 
-$dbh->disconnect;
+#$dbh->disconnect;
 
 
 
