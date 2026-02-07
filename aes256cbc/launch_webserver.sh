3a1868f5e7ae42cc269764e680e46cbf36c48645
dmz <dmz>
2026-02-07 03:46:42 +0000

    Need to create logs directory.

--- aes256cbc/launch_webserver.sh
+++ aes256cbc/launch_webserver.sh
@@ -23,5 +23,6 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plug
 # Run docker compose file docker-compose.yaml. This fill will download images and run the httpd (apache) and MariaDB containers in the background.
 sudo docker compose up -d
 
+mkdir -p ./logs
 # Make logs work by giving ownership of logs directory to apache
 sudo chown -R www-data ./logs
