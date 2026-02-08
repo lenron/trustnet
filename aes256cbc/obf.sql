082e301bb2a1e51aae70752c86a94c9fa521a41e
dmz <dmz>
2026-02-08 05:30:12 +0000

    Passlock delete EVENT now scheduled in MariaDB on container creation. Event tested as persisting through reboots.
    event_scheduler server system variable turned ON in both my.cnf and a command in obf.sql

--- aes256cbc/obf.sql
+++ aes256cbc/obf.sql
@@ -1,14 +1,18 @@
 -- MariaDB set up script for lenron-obf
 
--- Grant privileges to user (not sure if necessary)
---GRANT ALL PRIVILEGES ON *.* TO 'chatriwe_admin'@'localhost';
-
--- Create Table on existing dbase
+-- Create Table on database set up with docker-compose.yaml
 USE chatriwe_obf;
----CREATE TABLE IF NOT EXISTS obfuscation (fingerprint VARCHAR(64), data VARCHAR(1000), ip VARCHAR(16), timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(fingerprint) );
 
+-- Create table for obfuscated data.
 CREATE TABLE IF NOT EXISTS obfuscation (id INT NOT NULL AUTO_INCREMENT, fingerprint VARCHAR(64), data VARCHAR(1000), ip VARCHAR(16), timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id) );
 
+-- Create table for passlocks.
 CREATE TABLE IF NOT EXISTS passlock_table (id INT NOT NULL AUTO_INCREMENT, browser_id VARCHAR(64), passlock VARCHAR(64), timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id) );
 
+-- Enable event scheduler so events can be scheduled.
+SET GLOBAL event_scheduler = ON;   
+
+-- Every 1 minute remove all records older than 5 minutes.
+CREATE EVENT remove_expired_passlocks ON SCHEDULE EVERY 1 MINUTE
+DO DELETE FROM passlock_table WHERE timestamp < CURRENT_TIMESTAMP() - INTERVAL 5 MINUTE;
 
