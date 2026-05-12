25137c962b2b54027606a8da88c0de88c5e115ea
dmz <dmz>
2026-05-12 03:52:52 +0000

    deleting files from last commit because that's how git works i guess

--- aes256cbc/backup_mariadb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-#sudo docker exec -i pgo-mariadb mariadb-dump -u root -p'pwd' --single-transaction --all-databases | gzip > /home/maker/trustnet/aes256cbc/backups/db_$(date +%F:%H:%M:%S).sql.gz
-#sudo docker exec -i pgo-mariadb mariadb-dump -u root -p'pwd' --single-transaction --all-databases | gzip > /home/maker/trustnet/aes256cbc/backups/mariadb_$(date +%F).sql.gz
-sudo docker exec -i pgo-mariadb mariadb-dump -u root -p'pwd' --single-transaction --all-databases | gzip > /home/maker/trustnet/aes256cbc/backups/mariadb_backup_$(date +%F).sql.gz
-
