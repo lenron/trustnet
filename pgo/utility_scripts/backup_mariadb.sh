c44c5680ee00ba5b8fab7855c93c3b1b1147df90
dmz <dmz>
2026-05-13 06:31:06 +0000

    Creating long random hard to guess directory.

--- pgo/utility_scripts/backup_mariadb.sh
+++ pgo/utility_scripts/backup_mariadb.sh
@@ -6,5 +6,6 @@ set -e
 
 # Run docker command inside container named pgo-mariadb: sudo docker exec -i pgo-mariadb
 # 
-sudo docker exec -i pgo-mariadb mariadb-dump -u root -p'pwd' --single-transaction --all-databases | gzip > /home/maker/trustnet/pgo/htdocs/mariadb_backups/mariadb_backup_$(date +%F).sql.gz
+#sudo docker exec -i pgo-mariadb mariadb-dump -u root -p'pwd' --single-transaction --all-databases | gzip > /home/maker/trustnet/pgo/htdocs/mariadb_backups/mariadb_backup_$(date +%F).sql.gz
+sudo docker exec -i pgo-mariadb mariadb-dump -u root -p'pwd' --single-transaction --all-databases | gzip > /home/maker/trustnet/pgo/htdocs/ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA/mariadb_backup_$(date +%F).sql.gz
 
