464061e54b36ceae58c53142c420b318225998d6
dmz <dmz>
2026-05-14 18:05:35 +0000

    Encrypted backup wasn't getting placed in long, random directory because I was telling cronjob to use a relative path which it cannot do.

--- pgo/utility_scripts/backup_mariadb.sh
+++ pgo/utility_scripts/backup_mariadb.sh
@@ -3,7 +3,7 @@ set -e
 
 SHARED_PASSWORD="ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA"
 MARIADB_DUMP="/home/maker/trustnet/pgo/mariadb_backup_$(date +%F).sql.gz"
-ENCRYPTED_DUMP="htdocs/ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA/mariadb_backup_$(date +%F).sql.gz.enc"
+ENCRYPTED_DUMP="/home/maker/trustnet/pgo/htdocs/ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA/mariadb_backup_$(date +%F).sql.gz.enc"
 
 # Run docker command inside container named pgo-mariadb: sudo docker exec -i pgo-mariadb
 #sudo docker exec -i pgo-mariadb mariadb-dump -u root -p'pwd' --single-transaction --all-databases | gzip > /home/maker/trustnet/pgo/mariadb_backup_$(date +%F).sql.gz
