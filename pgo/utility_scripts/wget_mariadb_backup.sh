8fe99c4dcd7644a14731d4cc5e4c7a813a595d32
dmz <dmz>
2026-05-14 02:32:32 +0000

    variablizing wget

--- pgo/utility_scripts/wget_mariadb_backup.sh
+++ pgo/utility_scripts/wget_mariadb_backup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
-# -P tells wget to place acquired file in first parameter directory
-#wget -P /home/maker/trustnet/pgo/mariadb_restores https://obf.mkrsvr.org/mariadb_backups/mariadb_backup_$(date +\%F).sql.gz
-wget -OP /home/maker/trustnet/pgo/mariadb_restores https://obf.mkrsvr.org/ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA/mariadb_backup_$(date +%F).sql.gz.enc
+
+URL_LOCATION="https://obf.mkrsvr.org/ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA/mariadb_backup_$(date +%F).sql.gz.enc"
+SAVE_LOCATION="/home/maker/trustnet/pgo/mariadb_restores/mariadb_backup_$(date +%F).sql.gz.enc"
+
+# -O saves to specific file location (overwriting older existing file).
+wget -O $SAVE_LOCATION $URL_LOCATION
