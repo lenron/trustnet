7709c307c1b2a556d30bd27904d9da233517caff
dmz <dmz>
2026-05-14 02:13:55 +0000

    Adding bash variables to backup/restore scripts for readability.

--- pgo/utility_scripts/backup_mariadb.sh
+++ pgo/utility_scripts/backup_mariadb.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -e
 
-#sudo docker exec -i pgo-mariadb mariadb-dump -u root -p'pwd' --single-transaction --all-databases | gzip > /home/maker/trustnet/aes256cbc/backups/db_$(date +%F:%H:%M:%S).sql.gz
+SHARED_PASSWORD="ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA"
+MARIADB_DUMP="/home/maker/trustnet/pgo/mariadb_backup_$(date +%F).sql.gz"
+ENCRYPTED_DUMP="htdocs/ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA/mariadb_backup_$(date +%F).sql.gz.enc"
 
 # Run docker command inside container named pgo-mariadb: sudo docker exec -i pgo-mariadb
-#sudo docker exec -i pgo-mariadb mariadb-dump -u root -p'pwd' --single-transaction --all-databases | gzip > /home/maker/trustnet/pgo/htdocs/ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA/mariadb_backup_$(date +%F).sql.gz
-sudo docker exec -i pgo-mariadb mariadb-dump -u root -p'pwd' --single-transaction --all-databases | gzip > /home/maker/trustnet/pgo/mariadb_backup_$(date +%F).sql.gz
+#sudo docker exec -i pgo-mariadb mariadb-dump -u root -p'pwd' --single-transaction --all-databases | gzip > /home/maker/trustnet/pgo/mariadb_backup_$(date +%F).sql.gz
+sudo docker exec -i pgo-mariadb mariadb-dump -u root -p'pwd' --single-transaction --all-databases | gzip > $MARIADB_DUMP
 
 # Encrypt using openssl and shared key.
-#openssl enc -aes-256-cbc -e -pbkdf2 -in htdocs/ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA/mariadb_backup_2026-05-14.sql.gz -out htdocs/ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA/mariadb_backup_2026-05-14.sql.gz.enc -pass pass:l
-openssl enc -aes-256-cbc -e -pbkdf2 -in /home/maker/trustnet/pgo/mariadb_backup_$(date +%F).sql.gz -out htdocs/ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA/mariadb_backup_$(date +%F).sql.gz.enc -pass pass:ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA
+openssl enc -aes-256-cbc -e -pbkdf2 -in $MARIADB_DUMP -out $ENCRYPTED_DUMP -pass pass:$SHARED_PASSWORD
 
 # Cleanup backup file.
-rm /home/maker/trustnet/pgo/mariadb_backup_$(date +%F).sql.gz
+rm $MARIADB_DUMP
 
 
--- pgo/utility_scripts/restore_mariadb.sh
+++ pgo/utility_scripts/restore_mariadb.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+
+# Exit on error. Not sure if we want this enabled?
 set -e
 
+SHARED_PASSWORD="ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA"
+ENCRYPTED_FILE="/home/maker/trustnet/pgo/mariadb_restores/mariadb_backup_$(date +%F).sql.gz.enc"
+DECRYPTED_FILE="/home/maker/trustnet/pgo/mariadb_restores/mariadb_backup_$(date +%F).sql.gz"
+
 # Decrypt using openssl.
-#openssl enc -aes-256-cbc -e -pbkdf2 -in /home/maker/trustnet/pgo/mariadb_backup_$(date +%F).sql.gz -out htdocs/ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA/mariadb_backup_$(date +%F).sql.gz.enc -pass pass:ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA
-openssl enc -aes-256-cbc -d -pbkdf2 -in  /home/maker/trustnet/pgo/mariadb_restores/mariadb_backup_$(date +%F).sql.gz.enc -out /home/maker/trustnet/pgo/mariadb_restores/mariadb_backup_$(date +%F).sql.gz -pass pass:ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA
+#openssl enc -aes-256-cbc -d -pbkdf2 -in  /home/maker/trustnet/pgo/mariadb_restores/mariadb_backup_$(date +%F).sql.gz.enc -out /home/maker/trustnet/pgo/mariadb_restores/mariadb_backup_$(date +%F).sql.gz -pass pass:ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA
+openssl enc -aes-256-cbc -d -pbkdf2 -in $ENCRYPTED_FILE -out $DECRYPTED_FILE -pass pass:$SHARED_PASSWORD
 
 # pipe unzipped logical backup into mariadb instance running in pgo-mariadb docker container.
-gunzip -c /home/maker/trustnet/pgo/mariadb_restores/mariadb_backup_$(date +%F).sql.gz | sudo docker exec -i pgo-mariadb mariadb -u root -p'pwd' chatriwe_obf 
+#gunzip -c /home/maker/trustnet/pgo/mariadb_restores/mariadb_backup_$(date +%F).sql.gz | sudo docker exec -i pgo-mariadb mariadb -u root -p'pwd' chatriwe_obf 
+gunzip -c $DECRYPTED_FILE | sudo docker exec -i pgo-mariadb mariadb -u root -p'pwd' chatriwe_obf 
 
