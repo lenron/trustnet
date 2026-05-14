4abbe254b4971a3eb6ec3001607b6199c3617263
Your Name <you@example.com>
2026-05-14 05:13:44 +0000

    Adding username/password auth bypass for wget script.

--- pgo/utility_scripts/wget_mariadb_backup.sh
+++ pgo/utility_scripts/wget_mariadb_backup.sh
@@ -3,6 +3,8 @@ set -e
 
 URL_LOCATION="https://obf.mkrsvr.org/ya6K5EXJEN2TQW4VSvS0acE3SqmxyBDX4dJYZYFGRdXilEUG0ixZIHCOhkxHBY7nNPOz6FWSmoHVA/mariadb_backup_$(date +%F).sql.gz.enc"
 SAVE_LOCATION="/home/maker/trustnet/pgo/mariadb_restores/mariadb_backup_$(date +%F).sql.gz.enc"
+USERNAME="username"
+PASSWORD="password"
 
 # -O saves to specific file location (overwriting older existing file).
-wget -O $SAVE_LOCATION $URL_LOCATION
+wget --user $USERNAME --password $PASSWORD -O $SAVE_LOCATION $URL_LOCATION
