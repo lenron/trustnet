fb7931d391df82be13a115cf4f43e032df7f3c5c
Your Name <you@example.com>
2026-05-15 21:46:19 +0000

    restore_mariadb.sh will now create a log, append successful update reports to it. Intended to also be used for readonly_index "Last Backed Up" functionality.

--- pgo/utility_scripts/restore_mariadb.sh
+++ pgo/utility_scripts/restore_mariadb.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Exit on error. Not sure if we want this enabled?
+# Exit on error.
+# DO NOT TURN THIS OFF IN PROD.
+# Needs to be on for logs/last updated to report truthfully.
 set -e
 
-SHARED_PASSWORD="0WolcMhmnlGm+qhEBKqKRDrDL5uEC1+fna1usK3+Vj8L8KGjb2vbV1CiTXLLJvq6rr3xAviZnSuI"
 ENCRYPTED_FILE="/home/maker/trustnet/pgo/mariadb_restores/mariadb_backup_$(date +%F).sql.gz.enc"
 DECRYPTED_FILE="/home/maker/trustnet/pgo/mariadb_restores/mariadb_backup_$(date +%F).sql.gz"
+SHARED_PASSWORD="0WolcMhmnlGm+qhEBKqKRDrDL5uEC1+fna1usK3+Vj8L8KGjb2vbV1CiTXLLJvq6rr3xAviZnSuI"
 
 # Decrypt using openssl.
 openssl enc -aes-256-cbc -d -pbkdf2 -in $ENCRYPTED_FILE -out $DECRYPTED_FILE -pass pass:$SHARED_PASSWORD
@@ -14,3 +16,7 @@ openssl enc -aes-256-cbc -d -pbkdf2 -in $ENCRYPTED_FILE -out $DECRYPTED_FILE -pa
 # -c sends output to standard out, allowing piping into mariadb container in this case.
 gunzip -c $DECRYPTED_FILE | sudo docker exec -i pgo-mariadb mariadb -u root -p'pwd' chatriwe_obf 
 
+# Create log of date last updated. 
+TIMESTAMP=$(date +"%Y-%m-%d_%H:%M:%S")
+LOG_LOCATION="$HOME/trustnet/pgo/mariadb_restores/update_log.txt"
+echo -e "Last updated: $TIMESTAMP" >> $LOG_LOCATION
