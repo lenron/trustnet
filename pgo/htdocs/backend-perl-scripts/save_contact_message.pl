2e90898b08f96a97b7ec434ee25dde8bdb679a69
dmz <dmz>
2026-06-16 16:33:05 +0000

    Contact messages now 'Nikki' limited to 5/day. Total contacts / day set to =1000

--- pgo/htdocs/backend-perl-scripts/save_contact_message.pl
+++ pgo/htdocs/backend-perl-scripts/save_contact_message.pl
@@ -20,6 +20,8 @@ my $db_name = 'chatriwe_obf';
 my $db_contact_messages_table = 'contact_messages';
 my $db_username = get_first_line('/run/secrets/mariadb_login');
 my $db_pw = get_first_line('/run/secrets/mariadb_pw');
+my $contacts_today_per_ip_table = 'contacts_today_per_ip';
+my $contacts_on_date_table = 'contacts_on_date';
 
 # Did CGI catch an HTTP Request object ($q->param)?
 if ($q->param){
@@ -36,15 +38,45 @@ if ($q->param){
 
 	# Get database handler $dbh by successfully connecting to database.
 	my $dbh = DBI->connect("dbi:MariaDB:$db_name", $db_username, $db_pw);
-	my $response = $dbh->do("INSERT INTO $db_contact_messages_table (ip, message) VALUES (?, ?)", undef, $ip, $sanitized);
+	# Check hard limit for total contacts today.
+	my $total_contacts_today = get_total_contacts_today($dbh, $ip);
+	add_log_message("Total contacts today: $total_contacts_today");
+	if ($total_contacts_today >= 1000) {
+		# Lock out.
+		print $q->header();
+		print qq{{"contact_response":"TOTAL_CONTACTS_OVER_1000"}};
+		exit(0);
+	}
 
-	# Send email to admin.
-	send_email('aaronbreault@gmail.com', $sanitized, $ip);
-
-	# HTTP POST response requires a header.
-	print $q->header();
-	print qq{{"response":"$response"}};
+	# First check if we need to disallow the contact and ban the ip.
+	my $contacts_today = get_contacts_today_for_ip($dbh, $ip);
+	add_log_message("Contacts today for ip: $ip - $contacts_today");
+	if ($contacts_today >= 5) { # Nikki limit.
+		# Tell client ip they are contacting too much and are banned for 1 day.
+		print $q->header();
+		print qq{{"contact_response":"CONTACTS_OVER_5"}};
+	} else {
+		# Run the contact.
+		# Save message to contact message table.
+		my $save_contact_response = $dbh->do("INSERT INTO $db_contact_messages_table (ip, message) VALUES (?, ?)", undef, $ip, $sanitized);
+		# Send email to admin.
+		send_email('aaronbreault@gmail.com', $sanitized, $ip);
+		if ($save_contact_response == 1) {
+			# Run increment command for contacts_on_date (more logic to be added later when we set up site-wide shutdown).
+			$dbh->do("INSERT INTO $contacts_on_date_table (date_contacted, contacts_on_this_date) VALUES (CURRENT_DATE, 1) ON DUPLICATE KEY UPDATE contacts_on_this_date = contacts_on_this_date + 1;", undef);
+			# Run increment command for contacts_today_per_ip.
+			$dbh->do("INSERT INTO $contacts_today_per_ip_table (ip, contacts_today) VALUES (?, 1) ON DUPLICATE KEY UPDATE contacts_today = contacts_today + 1;", undef, $ip);
+			# Report success to client.
+			print $q->header();
+			print qq{{"contact_response":"SUCCESS"}};
+		} else {
+			# HTTP POST response requires a header.
+			print $q->header();
+			print qq{{"contact_response":"CONTACT_FAILED"}};
+		}
+	}
 }
+
 
 # Send message to inputted email using swaks, smtp.
 sub send_email {
@@ -102,6 +134,31 @@ sub get_first_line{
 	return $first_line;
 }
 
+# Get number of contacts today for a given ip.
+sub get_total_contacts_today {
+	my $dbh = shift;
+	my $ip = shift;
+	# DBI command for returning a single value with SELECT.
+	my $total_contacts_today = $dbh->selectrow_array("SELECT contacts_on_this_date FROM $contacts_on_date_table WHERE date_contacted = CURRENT_DATE", undef);
+	# Empty retun indicates record doesn't exist yet ==> nothing yet contacted today.
+	if ($total_contacts_today eq '') {
+		$total_contacts_today = 0;
+	}
+	return $total_contacts_today;
+}
+
+# Get number of contacts today for a given ip.
+sub get_contacts_today_for_ip {
+	my $dbh = shift;
+	my $ip = shift;
+	# DBI command for returning a single value with SELECT.
+	my $contacts_today = $dbh->selectrow_array("SELECT contacts_today FROM $contacts_today_per_ip_table WHERE ip = ?", undef, $ip);
+	# Empty retun indicates record doesn't exist yet ==> nothing yet contacted today.
+	if ($contacts_today eq '') {
+		$contacts_today = 0;
+	}
+	return $contacts_today;
+}
 
 
 
--- pgo/mariadb_setup_files/obf.sql
+++ pgo/mariadb_setup_files/obf.sql
@@ -23,6 +23,10 @@ CREATE TABLE IF NOT EXISTS access_failures_today_per_ip (id INT NOT NULL AUTO_IN
 CREATE TABLE IF NOT EXISTS unique_visitors_on_date (id INT NOT NULL AUTO_INCREMENT, date_visited DATE, unique_visitors_on_this_date INT, PRIMARY KEY(id), UNIQUE(date_visited));
 -- #5 - Daily unique visitor IPs - Not using UNIQUE here because I want a standard INSERT INTO to fail if ip exists (don't need response back).
 CREATE TABLE IF NOT EXISTS unique_visitor_ips_today (id INT NOT NULL AUTO_INCREMENT, ip VARCHAR(16), PRIMARY KEY(id));
+-- #6 - Contact messages today for a given ip..
+CREATE TABLE IF NOT EXISTS contacts_today_per_ip (id INT NOT NULL AUTO_INCREMENT, ip VARCHAR(16), contacts_today INT, PRIMARY KEY(id), UNIQUE(ip));
+-- #7 - Contact messages on this date
+CREATE TABLE IF NOT EXISTS contacts_on_date (id INT NOT NULL AUTO_INCREMENT, date_contacted DATE, contacts_on_this_date INT, PRIMARY KEY(id), UNIQUE(date_contacted));
 
 -- Enable event scheduler so events can be scheduled.
 SET GLOBAL event_scheduler = ON;   
@@ -35,6 +39,7 @@ DO DELETE FROM passlock_table WHERE timestamp < CURRENT_TIMESTAMP() - INTERVAL 6
 CREATE EVENT clear_stores_today_every_24_hours ON SCHEDULE EVERY 1 DAY STARTS CURRENT_DATE + INTERVAL '02:30:00' HOUR_SECOND DO TRUNCATE TABLE stores_today_per_ip;
 CREATE EVENT clear_access_failures_today_every_24_hours ON SCHEDULE EVERY 1 DAY STARTS CURRENT_DATE + INTERVAL '02:30:00' HOUR_SECOND DO TRUNCATE TABLE access_failures_today_per_ip;
 CREATE EVENT clear_unique_visitor_ips_every_24_hours ON SCHEDULE EVERY 1 DAY STARTS CURRENT_DATE + INTERVAL '02:30:00' HOUR_SECOND DO TRUNCATE TABLE unique_visitor_ips_today;
+CREATE EVENT clear_contactes_today_every_24_hours ON SCHEDULE EVERY 1 DAY STARTS CURRENT_DATE + INTERVAL '02:30:00' HOUR_SECOND DO TRUNCATE TABLE contacts_today_per_ip;
 
 -- grant privileges for mariadb-backup. (GRANT ALL doesn't work for these I guess.)
 --GRANT RELOAD, PROCESS ON *.* TO 'chatriwe_admin'@'%';   
