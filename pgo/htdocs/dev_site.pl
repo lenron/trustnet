4effc0f596d675caf9ac9477ff0b659bf13611c4
dmz <dmz>
2026-07-03 06:40:31 +0000

    adding html framework first draft for new one page dev site.

---
+++ pgo/htdocs/dev_site.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use CGI qw(:standard escapeHTML);
+# Use custom library.
+require "/usr/local/apache2/htdocs/backend-perl-scripts/pgolib.pl";
+
+# Create cgi object for html function.
+my $cgi = CGI->new;
+
+add_log_message_with_time_and_ip("DEV SITE ACCESSED");
+
+# HTTP Response requires proper header to work.
+print $cgi->header('text/html');
+# Read in static error file unless we have stored less than 50,000 times today.
+open my $fh, '<', 'dev_site.html';
+print while <$fh>;
+close $fh;   
+
+exit(0);
+
+
+
+
